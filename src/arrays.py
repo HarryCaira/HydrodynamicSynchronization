e079c5435783e119962690d77a95e0a9ccb11367
Harry Caira <harry@MacBookPro.mynet>
2025-11-02 21:23:00 +0000

    adds the arrays module, implementing the ParticleArray2D class

---
+++ src/arrays.py
@@ -0,0 +1,76 @@
+import numpy as np
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from src.constants import GlobalConstants
+
+
+class ParticleArray2DInterface(ABC):
+    @abstractmethod
+    def __init__(self, orbit_centers: np.array, positions: np.array) -> None: ...
+
+    @abstractmethod
+    def update_positions(self, new_positions: np.array) -> None: ...
+
+    @property
+    @abstractmethod
+    def positions(self) -> np.array: ...
+
+    @property
+    @abstractmethod
+    def orbit_centers(self) -> np.array: ...
+
+    @classmethod
+    @abstractmethod
+    def create(cls, nx: int, ny: int, constants: GlobalConstants) -> "ParticleArray2DInterface": ...
+
+
+class ParticleArray2D(ParticleArray2DInterface):
+    def __init__(self, orbit_centers: np.array, positions: np.array) -> None:
+        self._orbit_centers = orbit_centers
+        self._positions = positions
+
+    def update_positions(self, new_positions: np.array) -> None:
+        self._positions = new_positions
+
+    @property
+    def positions(self) -> np.array:
+        return self._positions
+
+    @property
+    def orbit_centers(self) -> np.array:
+        return self._orbit_centers
+
+    @classmethod
+    def create(
+        cls, nx: int, ny: int, constants: GlobalConstants, random_seed: Optional[int] = None
+    ) -> "ParticleArray2D":
+        """Create a 2D array of particles in a grid formation"""
+        orbit_centers = []
+        positions = []
+        phases = []
+
+        if random_seed is not None:
+            phases = np.random.uniform(0, 2 * np.pi, size=nx * ny)
+        else:
+            phases = np.zeros(nx * ny)
+
+        for i in range(nx):
+            for j in range(ny):
+                orbit_center = np.array(
+                    [i * constants.orbit_separation, j * constants.orbit_separation, 0.0]
+                )
+                orbit_centers.append(orbit_center)
+                phi = phases[i * ny + j]
+
+                radius = np.array(
+                    [
+                        constants.orbit_radius * np.cos(phi),
+                        constants.orbit_radius * np.sin(phi),
+                        0.0,
+                    ]
+                )
+                particle_position = orbit_center + radius
+                positions.append(particle_position)
+
+        return cls(np.array(orbit_centers).T, np.array(positions).T)
---
+++ tests/arrays_test.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from src.arrays import ParticleArray2D
+from src.constants import GlobalConstants
+
+class TestParticleArray2D:
+    def test__create__arrays_are_correct_shapes(self) -> None:
+        nx = 13
+        ny = 27
+        constants = GlobalConstants()
+        particle_array = ParticleArray2D.create(nx, ny, constants)
+
+        assert particle_array.orbit_centers.shape == (3, nx * ny)
+        assert particle_array.positions.shape == (3, nx * ny)
+
+    def test__create__arrays_contain_correct_data(self) -> None:
+        nx = ny = 2
+        r = 0.5
+        constants = GlobalConstants(orbit_radius=r, orbit_separation=3*r)
+        particle_array = ParticleArray2D.create(nx, ny, constants)
+
+        expected_orbit_centers = np.array(
+            [[0.0, 0.0, 0.0], [0.0, 1.5, 0.0], [1.5, 0.0, 0.0], [1.5, 1.5, 0.0]]
+        ).T
+        expected_positions = (
+            expected_orbit_centers
+            + np.array([[r, 0.0, 0.0], [r, 0.0, 0.0], [r, 0.0, 0.0], [r, 0.0, 0.0]]).T
+        )
+
+        np.testing.assert_array_almost_equal(particle_array.orbit_centers, expected_orbit_centers)
+        np.testing.assert_array_almost_equal(particle_array.positions, expected_positions)
+
+    def test__update_positions(self) -> None:
+        array = ParticleArray2D(orbit_centers=np.zeros((3, 2)), positions=np.zeros((3, 2)))
+        new_positions = np.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+        array.update_positions(new_positions)
+
+        np.testing.assert_array_equal(array.positions, new_positions)
+        np.testing.assert_array_equal(array.orbit_centers, np.zeros((3, 2)))
+
+    def test__positions(self) -> None:
+        positions = np.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+        array = ParticleArray2D(orbit_centers=np.zeros((3, 2)), positions=positions)
+
+        np.testing.assert_array_equal(array.positions, positions)
+
+    def test__orbit_centers(self) -> None:
+        orbit_centers = np.array([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+        array = ParticleArray2D(orbit_centers=orbit_centers, positions=np.zeros((3, 2)))
+
+        np.testing.assert_array_equal(array.orbit_centers, orbit_centers)
